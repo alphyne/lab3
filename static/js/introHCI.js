98e5171c02609e5da5b28419f3866710bb519061
Jared Suasin <jsuasin@ucsd.edu>
2018-01-24 19:50:33 -0800

    Add CSS Effects

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,7 +10,9 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	$("a.thumbnail").click(projectClick);
@@ -28,16 +30,9 @@ function initializePage() {
 		if (description.length == 0) {
 			$(containingProject).append("<div class = 'project-description'><p>Description of the project.</p><div>");
 		} else {
-			description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+			//description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+			$(description).fadeOut();
 		}
 		
-
-		//var description = $(containingProject.find(".project-description");
-		
-		/*if(description.length == 0) {
-			(containingProject).append("<div class='project-description'><p>Description of the project.</p><div>");
-		} else {
-			description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
-		}*/
 	}
 }
